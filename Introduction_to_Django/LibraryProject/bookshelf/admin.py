2e6916c196077872a66d05b7d231c34a49a405f2
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-18 21:22:09 +0100

    readme files created

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book     # import Book model
+
 
 # Register your models here.
+admin.site.register(Book)     #  Register the Book Model with the Django Admin
+
+@admin.register(Book)          # Another way to register Model to enable customizations
+class BookAdmin(admin.ModelAdmin):
+  list_display = ('title','author','publication_year')  
+  list_filter = ('publication_year','author')
+  search_fields = ('title','author')
