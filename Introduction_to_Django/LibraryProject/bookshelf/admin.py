10412bbe650198062615a12c4d2305f8e580bd73
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-18 21:25:23 +0100

    readme files created

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -7,6 +7,6 @@ admin.site.register(Book)     #  Register the Book Model with the Django Admin
 
 @admin.register(Book)          # Another way to register Model to enable customizations
 class BookAdmin(admin.ModelAdmin):
-  list_display = ('title','author','publication_year')  
-  list_filter = ('publication_year','author')
-  search_fields = ('title','author')
+  list_display = ('title','author','publication_year') # columns in the list view of your Book model in the admin interface.
+  list_filter = ('publication_year','author') # This attribute enables filters on the right sidebar of the admin list view.
+  search_fields = ('title','author') # 	This attribute adds a search box at the top of the admin list view.
