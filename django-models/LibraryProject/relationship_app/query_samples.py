81e90f4994d826c34527b5750ea246df23ee5d27
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-20 16:28:19 +0100

    submission of query files

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,6 +8,7 @@ books = Book.objects.get(author = 'author_name')
 print(books)
 
 # """ List all books in a Library """
+books = Library.objects.get(name="library_name")
 books.all()
 for book in books:
   print(book)
