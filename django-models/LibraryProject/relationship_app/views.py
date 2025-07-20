73c90c1548075685d2de182b6a4b1939e92f7ff7
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-20 17:44:53 +0100

    function book view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
+from relationship_app.models import Book
 
 # Create your views here.
+""" creating view for a book model """
+def book_list (request):
+  books = Book.objects.all()
+  list_of_books = (f"{books.title} and {books.author}")
+  return render (request,list_of_books)
