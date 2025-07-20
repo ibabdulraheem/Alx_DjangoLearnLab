fc37849c3a7d229d34949a383a9b71791a2cdea0
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-20 23:25:40 +0100

    user authentication added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse_lazy,path
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth import login
 # Create your views here.
 """ creating view for a book model """
 def book_list (request):
