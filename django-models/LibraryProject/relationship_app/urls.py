cdc77dcb8bf7f9fc7771f60fa23818438e9e1025
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-20 21:41:28 +0100

    url pattern configurations

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import book_list, BookListView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
   path ('hello/',views.book_list, name ='hello'),
