08cba65c21a7fb86d1501275bd8cd1ef53c74240
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-20 18:05:48 +0100

    html template for function view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,5 +6,4 @@ from relationship_app.models import Book
 """ creating view for a book model """
 def book_list (request):
   books = Book.objects.all()
-  template_name = 'list_books.html'
-  return render (books/template_name)
+  return render (request,books/book_list)
