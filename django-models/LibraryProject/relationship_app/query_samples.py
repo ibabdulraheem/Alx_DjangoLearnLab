b20644367ebc6f793482b78392c892b515663337
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-20 13:28:04 +0100

    submission of query file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,11 @@
-""" Query all books by specific author """
+
 from relationship_app.models import Book
 from relationship_app.models import Library
 from relationship_app.models import Librarian
 
-
-book = Book.objects.all(author = 'author')
-book.save()
+""" Query all books by specific author """
+books = Book.objects.get(author = 'author_name')
+print(books)
 
 """ List all books in the Library """
 library = Library.objects.all()
@@ -13,7 +13,7 @@ for books in library:
   print(books)
 
 """ Retrieve the Librarian for a Library """
-librarian = Librarian.objects.get(name = "library_name")
+librarian = Librarian.objects.get(name = "librarian_name")
 
 
 
