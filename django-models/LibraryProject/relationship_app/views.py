432081d2d377f82504feb88a9ff23eafe7868297
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-20 23:24:25 +0100

    user authentication added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,8 +5,10 @@ from relationship_app.models import Book
 from relationship_app.models import Library
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.urls import reverse_lazy
-
+from django.urls import reverse_lazy,path
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView,LogoutView
 # Create your views here.
 """ creating view for a book model """
 def book_list (request):
@@ -20,4 +22,18 @@ class BookListView(ListView):
     print(book)
   template_name = 'relationship_app/library_detail.html'
   success_url = reverse_lazy('book')
-  
+
+""" User registration view""" 
+class SignUpView(CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy('register.html')
+
+""" User Login View"""
+urlpatterns = [
+  path('login/',LoginView.as_view(template_name = 'relationship_app/login.html'), name = 'login'),
+]
+
+""" Use Logout View"""
+urlpatterns = [
+  path ('logout/',LogoutView.as_view(template_name = 'relationship_app/logout.html'), name ='logout')
+]
