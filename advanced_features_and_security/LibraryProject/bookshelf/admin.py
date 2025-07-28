f566e95d88c172b10be286b3d54d8e1d98d40e7b
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-28 21:25:35 +0100

    submission of custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,7 @@ from .models import CustomUser,CustomUserAdmin
 # Register your models here.
 
 admin.site.register (CustomUser,CustomUserAdmin)
+["admin.site.register(CustomUser, CustomUserAdmin)"]
 
 
 
