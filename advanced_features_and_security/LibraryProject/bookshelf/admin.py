dc6c9ed9148f6cc4151b9a4c9f84f193be0a9bea
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-07-28 21:01:51 +0100

    submission of custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import CustomUser,CustomUserManager,CustomUserAdmin
+from .models import CustomUser,CustomUserManager
 
 # Register your models here.
 
-admin.site.register(CustomUser ,CustomUserManager,CustomUserAdmin)
+admin.site.register(CustomUser )
+admin.site.register(CustomUserManager )
+
 
 
