b55c59cb9d584cc88b8a6aeff92810e3d9d2e5af
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-03 12:38:56 +0100

    api endpoint

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import BookList
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
 ]
