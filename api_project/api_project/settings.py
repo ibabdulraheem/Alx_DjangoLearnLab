e848f66a684f6ffcc991b9104d8d05af31bf4c66
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-05 15:44:08 +0100

    authentication setup

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -123,3 +123,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+""" Adding authentication """
+["rest_framework.authentication.TokenAuthentication"]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Optional: Keep for browsable API
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
