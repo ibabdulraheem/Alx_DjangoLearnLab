1eddee3f7abdce32b089a4c8280a67b43e4fc834
Ibrahim Abdulraheem <ibadul707@gmail.com>
2025-08-05 22:07:22 +0100

    setting up django and DRF

--- api/models.py
+++ api/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
-# Create your models here.
 
 """ creating 2 data models: Book and Author"""
 class Author(models.Model):
