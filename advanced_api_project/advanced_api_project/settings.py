c1e7eeb4aaa5fa12aca90e22aa331538a8747818
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-07 14:16:53 +0100

    adding model serializers

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4((e*=os48*m&a*@6e*!5=y6&tv5y5j@^wq_dqfgqhc-d2x!ld'
+SECRET_KEY = 'django-insecure-#q(!+*%rr6d+kvskwhk5oe*ur!exq)jg50&7wxvsc7q=$6#^$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
-""" creating 2 data models: Book and Author"""
-class Author(models.Model):
+""" Creating Author and Book models """
+class Author (models.Model):
   name = models.CharField(max_length=100)
 
-class Book(models.Model):
+
+class Book (models.Model):
   title = models.CharField(max_length=200)
   publication_year = models.IntegerField()
   author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
