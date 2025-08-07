3fd2ba53903634b7bad6e55f7c41843a8a799661
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-07 14:59:07 +0100

    Author and Book Serializers created

--- advanced_api_project/advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#q(!+*%rr6d+kvskwhk5oe*ur!exq)jg50&7wxvsc7q=$6#^$^'
+SECRET_KEY = 'django-insecure-b54mx*9cnfgoxma7%2i8uuyy71r&=wgz-jad*4qu34k*(y%52j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
