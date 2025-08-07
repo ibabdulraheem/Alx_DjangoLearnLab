983c86c61524212d54987eab4d44a228373311c7
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-07 15:47:22 +0100

    adv api submission

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Author, Book
+
+
+
+""" Creating Serializers for the Author and Book """
+
+class AuthorSerializer (serializers.ModelSerializer):
+  class meta:
+    model = Author
+    field = ['name']
+
+class BookSerializer (serializers.ModelSerializer):
+  class meta:
+    model = Book
+    field = '__all__'
