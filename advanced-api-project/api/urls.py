7eb2fe894d5d8756484d0049d1fbb66b727ce80e
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-07 22:19:41 +0100

    Adding url patterns

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    CustomBookListView,
+    CustomBookCreateView,
+    CustomBookDetailView,
+    CustomBookUpdateView,
+    CustomBookDeleteView
+)
+
+urlpatterns = [
+  #  # URL for listing all books and creating a new book
+    path('books/', CustomBookListView.as_view(), name='book-list'),
+    path('books/create/', CustomBookCreateView.as_view(), name='book-create'),
+
+    # URL for retrieving, updating, and deleting a specific book
+    path('books/<int:pk>/', CustomBookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', CustomBookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', CustomBookDeleteView.as_view(), name='book-delete'),
+]
