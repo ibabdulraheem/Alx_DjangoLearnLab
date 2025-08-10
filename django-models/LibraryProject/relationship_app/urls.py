b309b1e06fe38e014f2a094ea0f5f5a502424ef5
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-10 17:08:01 +0100

    adding templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import UserLoginView,UserLogoutView,BookListView
 urlpatterns = [
   path ('list_books/',views.list_books(template_name= list_books.html), name ='list_books'),
   path ('books_by_library/',views.BookListView.as_view(), name = 'book_by_library'),
-  path('login/',views.UserLoginView.as_view(template_name="login/login.html"), name="login"),
-  path ('logout/',views.UserLogoutView.as_view(template_name="logout/logout.html"), name="logout"),
-  path ('register/',views.SignUpView.as_view(template_name="register/register.html"), name="register"),
+  path('login/',views.UserLoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+  path ('logout/',views.UserLogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+  path ('register/',views.SignUpView.as_view(template_name="relationship_app/register.html"), name="register"),
 ]
