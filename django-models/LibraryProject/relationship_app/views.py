b922a69e347f729ab64506fd9bad24625ecf54f2
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-10 16:59:20 +0100

    adding templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,11 +13,11 @@ from django.contrib.auth import login
 # Create your views here.
 
 
-["relationship_app/list_books.html"]
+# ["relationship_app/list_books.html"]
 """ creating view for a book model """
 def list_books (request):
   books = Book.objects.all() 
-  return render(request, 'list_books.html')
+  return render(request, 'relationship_app/list_books.html')
 
 class BookListView(ListView):
   model = Library
