48516987a0ab07f6bda8cd12646d85ceb22fc555
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-10 16:35:55 +0100

    adding html file template for function-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,9 @@ from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib.auth import login
 # Create your views here.
+
+
+["relationship_app/list_books.html"]
 """ creating view for a book model """
 def list_books (request):
   books = Book.objects.all() 
