52020d67c7208187ac9218d6cf936e42db5c550c
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-10 14:27:29 +0100

    django advanced model file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,13 @@
 
 from relationship_app.models import Book
 from relationship_app.models import Library
-from relationship_app.models import Librarian
+from relationship_app.models import Librarian, Author
 
 """ Query all books by specific author """
-books = Book.objects.get(author = 'author_name').all()
-print(books)
+author = Author.objects.get(author='author_name')
+author.objects.filter(author='author')
+# books = Book.objects.get(author = 'author_name').all()
+# print(books)
 
 """ List all books in a Library """
 # ["Library.objects.get(name=library_name)"]
