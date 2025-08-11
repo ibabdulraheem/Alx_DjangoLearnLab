35a652131d440060d6fe66040b8b813247c44c4f
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-11 22:10:48 +0100

    adding usercreation form

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from blog import views as user_views
+
+
+
+urlpatterns = [
+
+path('register/', user_views.register, name='register'),
+path('login/',auth_views.LoginView.as_view(template_name = 'users/login.html'),name = 'login'),
+path('login/',auth_views.LogoutView.as_view(next_page = '/'),name = 'logout'),
+]
