f9f03fb6cd110dc3352ac16ed35c1a7d9e4cb666
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-11 13:41:36 +0100

    adding static templates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Creating Post and User Models for the blog App
+# class User(models.Model):
+#   name = models.CharField(max_length=200)
+  
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  published_date = models.DateTimeField(auto_now_add=True)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
