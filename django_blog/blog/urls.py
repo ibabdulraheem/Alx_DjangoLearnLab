319e77e62b90dca6e94f082f3dec7798cf63afa5
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-12 22:33:50 +0100

    adding url path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from blog import views as user_views
+from . import views
 
 
 
@@ -11,4 +12,9 @@ path('register/', user_views.register.as_view(template_name = 'blog/register.htm
 path('login/',auth_views.LoginView.as_view(template_name = 'blog/login.html'),name = 'login'),
 path('login/',auth_views.LogoutView.as_view(template_name = 'blog/logout.html'),name = 'logout'),
 path('profile/')
+path('', views.post_list, name='post_list'), #list all post
+path('new/', views.create_post, name='create_post'),  # create new post
+path('<int:pk>/', views.post_detail, name='post_detail'), # view single post
+path('<int:pk>/edit/', views.update_post, name='update_post'), # Edit existing post
+path('<int:pk>/delete/', views.delete_post, name='delete_post'), # Delete a post
 ]
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -21,7 +21,6 @@ from blog import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('login/',auth_views.LoginView.as_view(template_name = 'users/login.html'),name = 'login'),
-    path('login/',auth_views.LogoutView.as_view(next_page = '/'),name = 'logout'),
+    path('posts/', include('blog.urls')),
+   
 ]
