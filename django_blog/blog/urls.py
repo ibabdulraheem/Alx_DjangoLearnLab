c0c029276e33fdd933f958ba33d0538e4b7df4a0
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-12 16:52:12 +0100

    creating templates for login, logout and register

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 path('register/', user_views.register.as_view(template_name = 'blog/register.html'),name='register'),
 path('login/',auth_views.LoginView.as_view(template_name = 'blog/login.html'),name = 'login'),
 path('login/',auth_views.LogoutView.as_view(template_name = 'blog/logout.html'),name = 'logout'),
+path('profile/')
 ]
