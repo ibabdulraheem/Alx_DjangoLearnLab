b5133b8b80228ae449982487eef9ca21d27bf22b
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-12 16:23:39 +0100

    django advanced model file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,7 +7,7 @@ from blog import views as user_views
 
 urlpatterns = [
 
-path('register/', user_views.register, name='register'),
-path('login/',auth_views.LoginView.as_view(template_name = 'users/login.html'),name = 'login'),
-path('login/',auth_views.LogoutView.as_view(next_page = '/'),name = 'logout'),
+path('register/', user_views.register.as_view(template_name = 'blog/base.html'),name='register'),
+path('login/',auth_views.LoginView.as_view(template_name = 'blog/login.html'),name = 'login'),
+path('login/',auth_views.LogoutView.as_view(template_name = ''),name = 'logout'),
 ]
