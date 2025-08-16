14290093a0ce71824dcbc3c34c3a21ef5890429d
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-16 14:43:16 +0100

    adding Comment model to the blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -81,7 +81,32 @@ def update_post(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/update_post.html', {'form': form})
     
-    
+# CREATING VIEWS FOR A COMMENT_FORM CREATED INITIALLY IN THE forms.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    })
+   
 
 
 
