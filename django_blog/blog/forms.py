83b85a9247f3c44ca8759c561ae7cba13b5e4c64
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-16 15:12:16 +0100

    adding Comment model to the blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,6 @@
 # Creating CUSTOM REGISTRATION FORM and extend UserCreationForm to add the email field.
 from django import forms
-from .models import Post
+from .models import Post,Comment
 from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,5 +15,28 @@ class PostForm(forms.ModelForm):
     model = Post
     fields = ['title','content']
     
+# CREATING COMMENT FORM TO BE USED IN THE COMMENT VIEWS
+# ["CommentForm(forms.ModelForm)", "model = Comment"]
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']   # Only allow editing the text of a comment
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Write your comment here...',
+                'rows': 4
+            }),
+        }
+        labels = {
+            'text': 'Your Comment',
+        }
+
+    # Custom validation
+    def clean_text(self):
+        text = self.cleaned_data.get('text')
+        if len(text.strip()) < 5:
+            raise forms.ValidationError("Comment must be at least 5 characters long.")
+        return text
 
 
