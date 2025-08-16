d09ecfa1b78e39a3048b3692c7abc74a0fe9d2ed
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-16 14:30:39 +0100

    adding Comment model to the blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -11,3 +11,11 @@ class Post(models.Model):
   content = models.TextField()
   published_date = models.DateTimeField(auto_now_add=True)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#COMMENT MODEL
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  content = models.TextField()
+  created_at = models.DateTimeField()
+  updated_at = models.DateTimeField()
