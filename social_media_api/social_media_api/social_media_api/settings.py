71e90d82bd00177f4e9b4ef926b80b0f8a199c79
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-20 21:41:49 +0100

    adding custom user model

--- social_media_api/social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/social_media_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',    # for token authentication
     'accounts',
 ]
 
@@ -122,3 +123,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Telling django to use the model, CustomUser
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+INSTALLED_APPS = [
+    ...,
+    "rest_framework",
+    "rest_framework.authtoken",  # for token authentication
+    "accounts",
+]
+
+#Settingup DRF
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+
