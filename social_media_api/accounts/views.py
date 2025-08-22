ae2e3b3f1f87f44f39d9b4ff87cf17e3e013b034
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-22 15:15:21 +0100

    adding custom user model

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import generics,views
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
+from rest_framework import response
 
-# Create your views here.
+#Views for Registration endpoint
+User = get_user_model()
+class RegistrationView(generics.CreateApiView):
+  queryset = User.objects.all()
+  serializer_class = RegisterSerializer
+
+#Login View that return 'Token'
+class LoginView(ObtainAuthToken):
+  def post(self, request, *args, **kwargs):
+    return super().post(request, *args, **kwargs)
+  token = Token.objects.get(key = response.data['Token'])
+  Response({
+    'token': token.key,
+    'user_id': token.user_id,
+    'username': token.user.username
+  })
+
+
+
+
