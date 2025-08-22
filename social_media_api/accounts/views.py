95f99408951f6865ca8a48e1fec06eea70a1c307
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-22 15:48:52 +0100

    settins

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
-from rest_framework import response
+from rest_framework import response, permissions
 
 #Views for Registration endpoint
 User = get_user_model()
@@ -24,41 +24,13 @@ class LoginView(ObtainAuthToken):
     'username': token.user.username
   })
 
-#Profile View : show or update profile
-from rest_framework import generics, permissions
-from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
-from django.contrib.auth import get_user_model
-from .serializers import RegisterSerializer, UserSerializer
 
-User = get_user_model()
-
-# Registration
-class RegisterView(generics.CreateAPIView):
+  class ProfileView(generics.RetrieveUpdateAPIView):  # Profile View: show or update profile
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-
-# Login
-class LoginView(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({
-            "token": token.key,
-            "user_id": token.user_id,
-            "username": token.user.username
-        })
-
-
-class ProfileView(generics.RetrieveUpdateAPIView):  # Profile View: show or update profile
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
-
-#["from rest_framework.authtoken.models import Token", "serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user"]
 
 
