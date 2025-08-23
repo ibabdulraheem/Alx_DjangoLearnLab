0ed2f988ba1e957d450443416294ece3f88d118d
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-23 11:33:42 +0100

    adding endpoints for follow and unfollow

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
-from .views import FollowUserView, UnfollowUserView, ProfileView,FeedView
+from .views import FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -11,10 +11,6 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path ("api/", "posts.urls"),
-    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow"),
-    path("unfollow/<int:user_id>/", UnfollowUserView.as_view(), name="unfollow"),
-    path("profile/<int:user_id>/", ProfileView.as_view(), name="user-profile"), #Optional
-    path("feed/", FeedView.as_view(), name="feed"),
 ]
 
 #["unfollow/<int:user_id>/", "follow/<int:user_id>"]
