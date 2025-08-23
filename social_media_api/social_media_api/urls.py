154cf39818f0cbecb9fcd8e98641620fa41a5a23
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-23 12:11:00 +0100

    adding endpoints for follow and unfollow

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include ("posts.urls")), 
+    path("api/", include("posts.urls")), 
     path("api/accounts/", include("accounts.urls")),
     path("api/posts/", include("posts.urls")),
 ]
