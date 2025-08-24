48afcd0d0d75c2a84895fd2c8213b9fb5b6578f2
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-08-24 11:40:56 +0100

    adding

---
+++ social_media_api/notifications/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions,generics
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework.response import Response
+from django.db.models import Q
+
+
+
+
+# Custom permission: only allow authors to edit/delete their own objects
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user  # only author can modify
+
+#post view
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)    #Set the author to the logged-in user
+
+#comment view
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by("-created_at")
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+#Creating feed view
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):        
+        user = self.request.user           # current user
+        following_users = user.following.all()                 # posts by followed users
+        queryset = Post.objects.filter(author__in=following_users).order_by("-created_at")
+        return queryset
